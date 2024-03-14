54bf7fc83ddfb0612c3e221a1d4b2dc5877fc62a
Yann Djoumessi <yanndjoumessi@gmail.com>
2024-03-14 06:40:26 -0400

    Finish with the design of the sidebar

---
+++ src/components/tag/tag.component.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+import "./tag.styles.scss";
+
+const Tag = ({ label }) => {
+  return <div className="tag">{label}</div>;
+};
+
+export default Tag;
--- src/features/home/components/sidebar/sidebar.component.jsx
+++ src/features/home/components/sidebar/sidebar.component.jsx
@@ -3,9 +3,11 @@ import "./sidebar.styles.scss";
 import { Link } from "react-router-dom";
 import IconSVG from "../../../../components/icon-svg/icon-svg.component";
 import { useState } from "react";
+import Tag from "../../../../components/tag/tag.component";
 
 const Sidebar = () => {
   const [expanded, setExpanded] = useState(true);
+
   const [links, setLinks] = useState([
     {
       label: "home",
@@ -29,12 +31,16 @@ const Sidebar = () => {
       label: "Liked Songs",
       imageUrl: "	https://misc.scdn.co/liked-songs/liked-songs-64.png",
       category: "Playlist",
+      items: [1, 2, 3],
+      owner: "Dharld",
     },
     {
       label: "My hits",
       imageUrl:
         "https://i.scdn.co/image/ab67616d000011ebe85259a1cae29a8d91f2093d",
       category: "Playlist",
+      items: [1, 2, 3],
+      owner: "Dharld",
     },
   ]);
 
@@ -52,7 +58,7 @@ const Sidebar = () => {
   };
 
   return (
-    <div className={`wrapper ${expanded ? "expanded" : ""}`}>
+    <div className={`wrapper ${expanded ? "expanded" : ""} sidebar`}>
       <nav className="nav sidebar">
         <ul className="nav-ul">
           {links.map(({ to, label, active }) => {
@@ -74,25 +80,73 @@ const Sidebar = () => {
         </ul>
       </nav>
       <nav className="nav flex-1">
-        <ul className="nav-ul">
-          <li className="nav-li">
-            <div className="icon-wrap">
-              <IconSVG
-                styles="fill-white"
-                name={playlistIcon.label}
-                active={playlistIcon.active}
-                handleClick={handlePlaylistIcon}
-              />
+        <li className="nav-li">
+          <div className="icon-wrap playlist-icon" onClick={handlePlaylistIcon}>
+            <IconSVG
+              styles="fill-white"
+              name={playlistIcon.label}
+              active={playlistIcon.active}
+            />
+            {expanded && <span>Your Library</span>}
+          </div>
+          {expanded && (
+            <div className="buttons">
+              <div className="button-container">
+                <IconSVG
+                  styles="fill-white"
+                  name="add"
+                  active={true}
+                  size="small"
+                />
+              </div>
+              <div className="button-container">
+                <IconSVG
+                  styles="fill-white"
+                  name="arrowNext"
+                  active={true}
+                  size="small"
+                />
+              </div>
             </div>
-          </li>
-          <div className="playlists">
-            {collections.map(({ label, imageUrl }) => {
+          )}
+        </li>
+        {expanded && (
+          <>
+            <div className="tags">
+              <Tag label="Playlists" />
+            </div>
+            <div className="wrap parameters">
+              <IconSVG name="search" size="small" />
+              <div className="recents">
+                <span>Recents</span>
+                <IconSVG name="tracks" active={true} />
+              </div>
+            </div>
+          </>
+        )}
+        <ul className="nav-ul">
+          <div className={`playlists ${expanded ? "expanded" : ""}`}>
+            {collections.map(({ label, imageUrl, category, items }) => {
               return (
-                <Link to="playlist" className="nav-li" key={label}>
-                  <div className="icon-wrap">
-                    <img src={imageUrl} alt="" />
+                <li className="nav-li" key={label}>
+                  <div className="icon">
+                    <Link to="playlist" className="nav-li">
+                      <div className="icon-wrap">
+                        <img src={imageUrl} alt="" />
+                      </div>
+                      {
+                        <div className="informations">
+                          <div className="title">{label}</div>
+                          <div className="details">
+                            <span>{category}</span>
+                            <div className="dot"></div>
+                            <span>{items.length}</span>
+                          </div>
+                        </div>
+                      }
+                    </Link>
                   </div>
-                </Link>
+                </li>
               );
             })}
           </div>
